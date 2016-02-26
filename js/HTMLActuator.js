ee27e07f6427c17ecf997a0c021ab8792309662b
Lukas Subel <lukas@lsubel.de>
2016-02-27 00:25:35 +0100

    Added styling

--- js/HTMLActuator.js
+++ js/HTMLActuator.js
@@ -1,8 +1,10 @@
 function HTMLActuator() {
   this.languageSelection  = document.querySelector(".ln-selection");
   this.question           = document.querySelector(".question");
-  this.menuContainer      = document.querySelector(".menu-container");
-  this.questionContainer  = document.querySelector(".question-container");
+  this.menuContainer            = document.querySelector(".menu-container");
+  this.menuContainerFooter      = document.querySelector(".menu-container-footer");
+  this.questionContainer        = document.querySelector(".question-container");
+  this.questionContainerFooter  = document.querySelector(".question-container-footer");
 }
 
 HTMLActuator.prototype.addLanguageToMenu = function (ln, label){
@@ -18,16 +20,20 @@ HTMLActuator.prototype.setNewQuestion = function(id){
 
 HTMLActuator.prototype.showMenu = function(){
   this.menuContainer.classList.add("show-container");
+  this.menuContainerFooter.classList.add("show-container");
 }
 
 HTMLActuator.prototype.hideMenu = function(){
   this.menuContainer.classList.remove("show-container");
+  this.menuContainerFooter.classList.remove("show-container");
 }
 
 HTMLActuator.prototype.showQuestion = function(){
   this.questionContainer.classList.add("show-container");
+  this.questionContainerFooter.classList.add("show-container");
 }
 
 HTMLActuator.prototype.hideQuestion = function(){
-this.questionContainer.classList.remove("show-container");
+  this.questionContainer.classList.remove("show-container");
+  this.questionContainerFooter.classList.remove("show-container");
 }
