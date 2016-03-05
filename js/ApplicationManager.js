f06dbee2835a673b4572fd26c0e24fb9dce621cb
Lukas Subel <lukas@lsubel.de>
2016-03-05 16:08:08 +0100

    Changed color to another eye-friendly one

--- js/ApplicationManager.js
+++ js/ApplicationManager.js
@@ -25,7 +25,7 @@ function ApplicationManager(InputManager, Actuator, StorageManager, TranslationM
   this.first_available_language   = undefined;
   this.available_colors           = [
     "#6C7A89", "#95a5a6", "#ABB7B7", "#BDC3C7",
-    "#913D88", "#BF55EC", "#9b59b6", "#BE90D4",
+    "#D2527F", "#BF55EC", "#9b59b6", "#BE90D4",
     "#22A7F0", "#3498db", "#2980b9", "#3A539B",
     "#2ecc71", "#27ae60", "#1abc9c", "#16a085",
     "#f1c40f", "#f9bf3b", "#F5AB35", "#e9d460",
