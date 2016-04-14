f6dfa8b3daef180d10704127ee709e3a7c51930e
lsubel <lukas@lsubel.de>
2016-04-14 12:31:13 +0200

    Remove phonegap.js b/c it is not necessary anymore

--- js/phonegap.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("deviceready", function(){
-  devlog("Device ready");
-  var link = document.querySelector(".href-sourcecode");
-  link.addEventListener("click", function(evt){
-      devlog("User clicked on source code URL - opening browser");
-      evt.preventDefault();
-      var ref = cordova.InAppBrowser.open("https://github.com/lsubel/amam", "_system", "location=yes");
-  });
-});
